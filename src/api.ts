b4840a8f840ebe1c473835a8c0e1e84deafe6c9e
yujinpan <1192878390@qq.com>
2023-06-08 10:27:57 +0800

    Update api types

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,24 @@
 import request from '@/request';
 
 export function chatCompletions(role: string, content: string) {
-  return request.post(`/chatgpt`, {
+  return request.post<{
+    id: string;
+    object: 'chat.completion';
+    created: number;
+    choices: {
+      index: number;
+      message: {
+        role: number;
+        content: string;
+      };
+      finish_reason: 'stop';
+    }[];
+    usage: {
+      prompt_tokens: number;
+      completion_tokens: number;
+      total_tokens: number;
+    };
+  }>(`/chatgpt`, {
     model: 'gpt-3.5-turbo',
     messages: [{ role, content }],
   });
