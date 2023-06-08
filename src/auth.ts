38b87efc3fc5a59fb5dae22e2bee14c30b00d9b7
yujinpan <1192878390@qq.com>
2023-06-08 22:37:31 +0800

    feat: add encrypt for keys

--- src/auth.ts
+++ src/auth.ts
@@ -1,15 +1,15 @@
+import { getAuthKey } from './secret';
+
 const LOCAL_AUTH_KEY = 'LOCAL_AUTH_KEY';
 
 export function validateLocalAuthKey() {
-  return (
-    localStorage.getItem(LOCAL_AUTH_KEY) === import.meta.env.VITE_APP_AUTH_KEY
-  );
+  return localStorage.getItem(LOCAL_AUTH_KEY) === getAuthKey();
 }
 
 export function validateAuthKey(code?: string) {
   if (validateLocalAuthKey()) return true;
 
-  const state = import.meta.env.VITE_APP_AUTH_KEY === code;
+  const state = getAuthKey() === code;
   if (state) {
     localStorage.setItem(LOCAL_AUTH_KEY, code);
   }
