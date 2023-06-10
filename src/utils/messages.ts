3ad70f968ebad185192f3bda53fb06845f45d4a5
yujinpan <1192878390@qq.com>
2023-06-10 15:44:43 +0800

    fix: messages error

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -1,5 +1,6 @@
 import { COMMAND } from './command';
 import { appStore } from './store';
+import { GPT_MODEL } from '../config';
 
 export function generateValidateMsg() {
   return `Please enter your activation code.`.trim();
@@ -17,8 +18,8 @@ export function generateHelpMsg() {
   return `
 Commands:
 
-- \`${COMMAND.GPT4}\` use gpt4 model
-- \`${COMMAND.GPT3_5}\` use gpt-3-turbo model
+- \`${COMMAND.GPT4}\` use ${GPT_MODEL.GPT4} model
+- \`${COMMAND.GPT3_5}\` use ${GPT_MODEL.GPT3_5} model
 - \`${COMMAND.CLEAR}\` clear history
 - \`${COMMAND.HELP}\` show helper
 `.trim();
