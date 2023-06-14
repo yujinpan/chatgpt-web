4867856fead80d2a842d4e56c2681aaf202b6cb1
yujinpan <1192878390@qq.com>
2023-06-14 22:04:45 +0800

    fix: highlight load error when use Markdown code

--- src/components/use-markdown.ts
+++ src/components/use-markdown.ts
@@ -4,11 +4,15 @@ import { removeAttr } from '../utils/format';
 
 export function useMarkdown(props: { content: string; typing: boolean }) {
   const markdownHtml = ref('');
-  const loading = ref(false);
   const markdownContent = computed(() => {
+    const content = props.content || '';
     return (
-      (props.content || '') +
-      (props.typing || loading.value ? '<span class="typing">_</span>' : '')
+      content +
+      (props.typing
+        ? (content.match(/```/g)?.length || 0) % 2
+          ? ' _'
+          : '<span class="typing"> _</span>'
+        : '')
     );
   });
 
@@ -24,15 +28,8 @@ export function useMarkdown(props: { content: string; typing: boolean }) {
 
   const load = () => {
     const langs = getLangs(markdownContent.value);
-
     if (langs?.length) {
-      loading.value = true;
-      highlightCode(langs)
-        .then(
-          (highlight) => renderMarkdown(highlight),
-          () => renderMarkdown(),
-        )
-        .finally(() => (loading.value = false));
+      getHighlighter(langs).then((highlight) => renderMarkdown(highlight));
     } else {
       renderMarkdown();
     }
@@ -42,7 +39,6 @@ export function useMarkdown(props: { content: string; typing: boolean }) {
 
   return {
     markdownHtml,
-    loading,
   };
 }
 
@@ -50,13 +46,30 @@ function getLangs(text: string) {
   return text.match(/```[\w-]+/g)?.map((item) => item.replace('```', ''));
 }
 
-function highlightCode(langs: string[]): Promise<(str, lang) => string> {
-  return window.shiki
-    .getHighlighter({
+let highlighter;
+let highlighterPromise;
+const langsCache: Record<string, Promise<any>> = {};
+
+async function getHighlighter(langs: string[]): Promise<(str, lang) => string> {
+  if (!highlighterPromise) {
+    highlighterPromise = window.shiki.getHighlighter({
       theme: 'material-theme-palenight',
-      langs,
-    })
-    .then((highlighter) => (str, lang) => {
-      return removeAttr(highlighter.codeToHtml(str, { lang }), 'pre', 'style');
+      langs: [],
     });
+  }
+
+  highlighter = highlighter || (await highlighterPromise);
+
+  await Promise.all(
+    langs.map(
+      (item) =>
+        (langsCache[item] = langsCache[item] || highlighter.loadLanguage(item)),
+    ),
+  );
+
+  return highlighterWrapper;
+}
+
+function highlighterWrapper(str, lang) {
+  return removeAttr(highlighter.codeToHtml(str, { lang }), 'pre', 'style');
 }
