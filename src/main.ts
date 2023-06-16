bcaa8cf43887a94e0c447f3ddf4a0b8689f986fb
yujinpan <1192878390@qq.com>
2023-06-16 18:36:23 +0800

    feat: remove update local data

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,5 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 import './styles/index.scss';
-import { updateVersion } from './utils/version';
-
-updateVersion();
 
 createApp(App).mount('#app');
