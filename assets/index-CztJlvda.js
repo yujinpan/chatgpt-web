bd06be13b2d70f93e8b8e1d2b22dca384b921705
yujinpan <1192878390@qq.com>
2024-06-13 23:05:47 +0800

    deploy

---
+++ assets/index-CztJlvda.js
@@ -0,0 +1,2 @@
+const __vite__fileDeps=["webkit.css","App.css"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
+import{d as e,_ as t,c as o}from"../webkit.js";const i=e(()=>t(()=>import("../App.js"),__vite__mapDeps([0,1])));o(i).mount("#chatgpt-web");window.ChatGPIWebKit=()=>t(()=>import("../webkit.js").then(_=>_.D),__vite__mapDeps([0]));
