301ee4912c44b27e87121ca51b1dd88fe59cd264
yujinpan <1192878390@qq.com>
2024-06-14 09:23:18 +0800

    deploy

---
+++ assets/index-DfRNOFDK.js
@@ -0,0 +1,2 @@
+const __vite__fileDeps=["webkit.css"],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
+import{c as a,_ as e,C as _}from"../webkit.js";a(_).mount("#chatgpt-web");window.ChatGPIWebKit=()=>e(()=>import("../webkit.js").then(t=>t.D),__vite__mapDeps([0]));
